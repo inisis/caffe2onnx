fix softmax bug

converter/onnx/caffe_parser.py
@@ -243,12 +243,32 @@ class caffe2onnx_converter:
 
                 self._node_post_process(gemm_layer)
             elif layer.type == "Softmax":
-                softmax_layer = ops.SoftmaxLayer(layer)
-                softmax_layer._in_names.extend(list(layer.bottom))
-                softmax_layer._out_names.extend(list(layer.top))
+                permute_layer = ops.PermuteLayer(layer, "_permute")
+                shape = self.caffe_net.blobs[layer.bottom[0]].data.shape
+                permute_out_name = layer.name + "_permute_out"
+                permute_layer._in_names.extend(list(layer.bottom))
+                permute_layer._out_names.append(permute_out_name)
+                permute_layer.generate_node(shape)
+
+                self._node_post_process(permute_layer)
+
+                softmax_layer = ops.SoftmaxLayer(layer, "_softmax")
+                softmax_out_name = layer.name + "_softmax_out"
+                softmax_layer._in_names.append(permute_out_name)
+                softmax_layer._out_names.append(softmax_out_name)
                 softmax_layer.generate_node()
 
                 self._node_post_process(softmax_layer)
+
+                permute_layer = ops.PermuteLayer(layer, "_post_permute")
+                shape = self.caffe_net.blobs[layer.bottom[0]].data.shape
+                permute_out_name = layer.name + "_permute_out"
+                permute_layer._in_names.append(softmax_out_name)
+                permute_layer._out_names.extend(list(layer.top))
+                permute_layer.generate_node(shape)
+
+                self._node_post_process(permute_layer)
+
             elif layer.type == "Sigmoid":
                 sigmoid_layer = ops.SigmoidLayer(layer)
                 sigmoid_layer._in_names.extend(list(layer.bottom))
@@ -563,7 +583,7 @@ class caffe2onnx_converter:
         onnx_rt_dict = {}
         for input_name in self.caffe_net.inputs:
             shape = self.caffe_net.blobs[input_name].shape
-            input_data = np.random.rand(*shape).astype(np.float32)
+            input_data = np.ones(shape).astype(np.float32)
             shape_str = " ".join(str(e) for e in shape)
             self.caffe_net.blobs[input_name].data[...] = input_data
             logging.info("caffe input: " + input_name + "shape: " + shape_str)

converter/onnx/layers/permute_layer.py
@@ -7,20 +7,32 @@ from layers.base_layer import BaseLayer
 
 
 class PermuteLayer(BaseLayer):
-    def __init__(self, layer):
-        super(PermuteLayer, self).__init__(layer)
+    def __init__(self, layer, name=None):
+        super(PermuteLayer, self).__init__(layer, name)
 
     def get_permute_attr(self, shape):
         attr_dict = {"perm": []}
-        if len(self._layer.permute_param.order) != len(shape):
-            order_list = np.arange(len(shape)).tolist()
-            permute_order = self._layer.permute_param.order
-            for element in permute_order:
-                order_list.remove(element)
-            permute_order.extend(order_list)
-            attr_dict["perm"] = permute_order
-        else:
-            attr_dict["perm"] = self._layer.permute_param.order
+        if self._layer.type == "Permute":
+            if len(self._layer.permute_param.order) != len(shape):
+                order_list = np.arange(len(shape)).tolist()
+                permute_order = self._layer.permute_param.order
+                for element in permute_order:
+                    order_list.remove(element)
+                permute_order.extend(order_list)
+                attr_dict["perm"] = permute_order
+            else:
+                attr_dict["perm"] = self._layer.permute_param.order
+        elif self._layer.type == "Softmax":
+            axis = self._layer.softmax_param.axis
+            index = np.arange(len(shape))
+            index[axis], index[-1] = index[-1], index[axis]
+            attr_dict["perm"] = index
+
+        return attr_dict
+
+    def get_softmax_permute_attr(self, params=None):
+        attr_dict = {"perm": []}
+        attr_dict["perm"] = params
 
         return attr_dict
 