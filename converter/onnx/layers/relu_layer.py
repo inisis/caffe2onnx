50646eb3aa5682679480bc01ee6ad524c503a4f6
desmond <desmond.yao@buaa.edu.cn>
2021-11-04 17:09:20 +0800

    fix relu bug

--- converter/onnx/layers/relu_layer.py
+++ converter/onnx/layers/relu_layer.py
@@ -8,9 +8,16 @@ class ReluLayer(BaseLayer):
     def __init__(self, layer, name=None):
         super(ReluLayer, self).__init__(layer, name)
 
+    def get_relu_attr(self):
+        attr_dict = {"alpha": 0}
+        attr_dict["alpha"] = self._layer.relu_param.negative_slope
+
+        return attr_dict
+
     def generate_node(self):
+        attr_dict = self.get_relu_attr()
         node = helper.make_node(
-            "Relu", self._in_names, self._out_names, self._layer.name
+            "LeakyRelu", self._in_names, self._out_names, self._layer.name, **attr_dict
         )
         logging.info("relu_layer: " + self._layer.name + " created")
         self._node = node
